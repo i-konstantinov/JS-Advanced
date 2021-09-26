9a825e0d920cbb74264c9e1440527c5a8a021f21
ivan-k <ivankonstantwork@gmail.com>
2021-09-26 17:32:43 +0300

    1. Fruit

---
+++ syntax funtions statements/01 syntax lab/strLen.js
@@ -0,0 +1,9 @@
+function strlen(a, b, c) {
+    let total = a.length + b.length + c.length;
+    let avg = Math.floor(total / 3);
+
+    console.log(total);
+    console.log(avg);
+}
+
+strlen('chocolate', 'ice cream', 'cake');
--- syntax funtions statements/fruit.js
+++ syntax funtions statements/fruit.js
@@ -1,1 +1,8 @@
-let a = 5
+function cost(name, weight, price) {
+    let weight_in_kg = weight / 1000;
+    let money = weight_in_kg * price;
+    console.log(`I need $${money.toFixed(2)} to buy ${weight_in_kg.toFixed(2)} kilograms ${name}.`)
+}
+
+
+cost('orange', 2500, 1.80);
