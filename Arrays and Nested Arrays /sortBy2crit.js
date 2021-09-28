88635eff93a09c7a38fb88066b0e58ff47484f68
ivan-k <ivankonstantwork@gmail.com>
2021-09-28 18:44:02 +0300

    8.sortBy2crit

---
+++ Arrays and Nested Arrays /sortBy2crit.js
@@ -0,0 +1,19 @@
+function solve(arr) {
+    let result = arr.sort((a,b) => {
+        if (a.length > b.length) {
+            return 1;
+        }
+        else if (a.length == b.length) {
+            return a.localeCompare(b);
+        }
+        else {
+            return -1;
+        }
+    })
+    console.log(result.join("\n"));
+}
+
+solve(['test', 
+'Deny', 
+'omen', 
+'Default'])
