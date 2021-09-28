01215990fc1f3dcac1b607a462ac7ad1c66a2e6e
ivan-k <ivankonstantwork@gmail.com>
2021-09-28 22:15:01 +0300

    10.

--- Arrays and Nested Arrays /ticTacToe.js
+++ Arrays and Nested Arrays /ticTacToe.js
@@ -1,16 +1,5 @@
 function solve(moves) {
-    function checkMoves(arr, fieldLen) {
-        newArr = [];
-        for (let i=0; i<arr.length; i++) {
-            let [x, y] = arr[i];
-            if (x<fieldLen && y<fieldLen) {
-                newArr.push(arr[i]);
-            }
-        }
-        return newArr;
-    }
-
-    function checkForWin(field, coordinates) {
+        function checkForWin(field, coordinates) {
         let [x,y] = coordinates;
         let sign = field[x][y];
         let hasWon = false;
@@ -38,7 +27,7 @@ function solve(moves) {
         }
     }       
 
-    let gameWon = false;
+    let gameWin = false;
 
     let board = [
         [false, false, false],
@@ -46,30 +35,31 @@ function solve(moves) {
         [false, false, false],   
     ]
 
-    moves = checkMoves(moves, board.length);
-
     let currentPl = 'X';
 
     for (let i = 0; i<moves.length; i++) {
+
         let [x, y]= moves[i].split(' ');
 
-        if (board[x][y]) {
-            console.log("This place is already taken. Please choose another!");
-        }
-        else {
-            board[x][y] = currentPl;
-            
-            if (checkForWin(board, [x,y]) == true) {
-                gameWon = true;
-                console.log(`Player ${currentPl} wins!`);
-                printResult(board);
-                break;
+        if (x<board.length && y<board.length) {
+            if (board[x][y]) {
+                console.log("This place is already taken. Please choose another!");
+            }  
+            else {
+                board[x][y] = currentPl;
+                    
+                if (checkForWin(board, [x,y]) == true) {
+                    gameWin = true;
+                    console.log(`Player ${currentPl} wins!`);
+                    printResult(board);
+                    break;
+                }
+                currentPl = currentPl == 'X' ? 'O' : 'X';
             }
-            currentPl = currentPl == 'X' ? 'O' : 'X';
-        }
+        } 
     }
-
-    if (!gameWon) {
+        
+    if (!gameWin) {
         console.log("The game ended! Nobody wins :(");
         printResult(board);
     }
@@ -77,11 +67,11 @@ function solve(moves) {
 
 solve(["0 1",
 "0 0",
-"0 2",
+"0 2", 
 "2 0",
 "1 0",
-"1 2",
 "1 1",
-"2 1",
+"1 2",
 "2 2",
+"2 1",
 "0 0"]);
