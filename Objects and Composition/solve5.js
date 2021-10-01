5496a938e3620d3535ee9a2e48f78a7759fcc3ba
ivan-k <ivankonstantwork@gmail.com>
2021-10-01 20:41:25 +0300

    Lowest Prices in Cities

---
+++ Objects and Composition/solve5.js
@@ -0,0 +1,27 @@
+function lowest(arr) {
+    let catalogue = {};
+
+    arr.forEach((element) => {
+        let [town, product, price] = element.split(" | ");
+        price = Number(price);
+
+        if (!catalogue[product]) {
+            catalogue[product] = {};
+        }
+        catalogue[product][town] = price;
+               
+    });
+    for (let prod in catalogue) {
+        let sorted = Object.entries(catalogue[prod]).sort((a,b) => a[1]- b[1]);
+        console.log(`${prod} -> ${sorted[0][1]} (${sorted[0][0]})`);
+    }
+}
+
+lowest(['Sample Town | Sample Product | 1010',
+'Sample Town | Orange | 2',
+'Sample Town | Sample Product | 1000',
+'Sample Town | Peach | 1',
+'Sofia | Orange | 3',
+'Sofia | Peach | 2',
+'New York | Sample Product | 1000.1',
+'New York | Burger | 10']);
